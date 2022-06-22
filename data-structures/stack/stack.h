de04c0eca6b460752ee0b73aece775cc1330c500
Jesse Palmer <jesselpalmer@gmail.com>
2022-06-22 11:31:53 -0700

    chore: init commit

---
+++ data-structures/stack/stack.h
@@ -0,0 +1,13 @@
+//
+// Created by Jesse Palmer on 6/22/22.
+//
+
+#ifndef CADT_STACK_H
+#define CADT_STACK_H
+
+
+class Stack {
+public:
+};
+
+#endif //CADT_STACK_H
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "data-structures/stack/stack.h"
+
+int main() {
+  Stack s = Stack();
+  std::cout << "hello world!" << std::endl;
+  return 0;
+}
