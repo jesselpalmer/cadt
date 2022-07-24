568403c7f9c75f97cfe451530506d9eaf419da88
Jesse Palmer <jesselpalmer@gmail.com>
2022-07-24 11:26:55 -0700

    test: add linkedlist test

--- test/linked_list_test.cpp
+++ test/linked_list_test.cpp
@@ -3,11 +3,32 @@
 //
 
 #include "linked_list_test.h"
+#include "../src/data_structures/linked_list.h"
+#include "../lib/testing/test_assert.h"
 
 namespace test {
 
-void LinkedListTest::loadTests() {
+class SizeTests : public testing::Test {
+ public:
+  using Test::Test;
+  void executeTest() override;
+};
 
+void SizeTests::executeTest() {
+  std::string testName = SizeTests::getTestName();
+  data_structures::LinkedList linkedList = data_structures::LinkedList();
+
+  linkedList.addToFront(1);
+  linkedList.addToFront(30);
+  linkedList.addToFront(23);
+  int size = linkedList.size();
+  testing::Assert(2, size, testName);
+}
+
+void LinkedListTest::loadTests() {
+  test::SizeTests* sizeTests = new test::SizeTests("should set the size correctly");
+  std::vector<testing::Test*> linkedListTests = {sizeTests};
+  test::LinkedListTest::addTests(linkedListTests);
 }
 
 LinkedListTest::LinkedListTest() {
