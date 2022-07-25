157945a19add1eb540e3e4f018b0b8a04397a5ea
Jesse Palmer <jesselpalmer@gmail.com>
2022-07-25 09:13:19 -0700

    refactor: update comments

--- test/linked_list_test.cpp
+++ test/linked_list_test.cpp
@@ -1,6 +1,7 @@
+// Copyright Jesse Palmer All Rights Reserved.
 //
-// Created by Jesse Palmer on 7/22/22.
-//
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file at https://github.com/jesselpalmer/cadt/LICENSE
 
 #include "linked_list_test.h"
 #include "../src/data_structures/linked_list.h"
--- test/linked_list_test.h
+++ test/linked_list_test.h
@@ -1,6 +1,7 @@
+// Copyright Jesse Palmer All Rights Reserved.
 //
-// Created by Jesse Palmer on 7/22/22.
-//
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file at https://github.com/jesselpalmer/cadt/LICENSE
 
 #ifndef CADT_TEST_LINKED_LIST_TEST_H_
 #define CADT_TEST_LINKED_LIST_TEST_H_
