9f54058c45de89cd4be17ac91e9c4a80773c1ae4
Jesse Palmer <jesselpalmer@gmail.com>
2022-08-11 09:34:44 -0700

    test: add deque test stubs

---
+++ test/deque_test/deque_test.h
@@ -0,0 +1,17 @@
+// Copyright Jesse Palmer All Rights Reserved.
+//
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file at https://github.com/jesselpalmer/cadt/blob/main/LICENSE
+
+#ifndef CADT_TEST_DEQUE_TEST_DEQUE_TEST_H_
+#define CADT_TEST_DEQUE_TEST_DEQUE_TEST_H_
+
+namespace deque_test {
+
+class DequeTest {
+
+};
+
+} // namespace deque_test
+
+#endif // CADT_TEST_DEQUE_TEST_DEQUE_TEST_H_
